dca54478114d00aeda00dab137dce17f928d7781
Ahmadreza Khederzadeh <rez@ketab-14.local>
2022-04-16 00:15:19 -0700

    item id added

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,40 @@
+import { useEffect, useState } from "react";
 import DefaultLayout from "../../components/layout/defaultLayout";
+import api from "../../utils/api";
+
 export function Home(){
+  const [loading, setLoading] = useState(false);
+  const [assets, setAssets] = useState([]);
+  useEffect(function(){
+    async function getApi(){
+      try{
+        setLoading(true);
+        const response = await api.get('assets');
+        console.log(response);
+        setAssets(response.data.data);
+        setLoading(false);
+      }catch(e){
+        setLoading(false);
+
+      }
+    }
+    getApi();
+  },[])
+  function renderFarm(){
+    return assets.map(function(item){
+      return(
+        <li key={item.id}> {item.id}</li>
+      )
+    })
+  
+  }
     return (
       <DefaultLayout>
+        <div style= {{display: loading ? "block": "none"}}>loading</div>
           <p> home </p>
+          <ul>
+          {renderFarm()}
+          </ul>
       </DefaultLayout>
     )
 }
